c1a7cf0f00bd2cc34be4a7ecde4ff3bf06ce7e1b
Muhammad Sahlan <muhammadsahlan2008@gmail.com>
2026-02-20 16:58:27 +0700

    Create dynamodb.js

---
+++ services/dynamodb.js
@@ -0,0 +1,15 @@
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient } = require('@aws-sdk/lib-dynamodb');
+
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION || 'us-east-1',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN
+  }
+});
+
+const dynamo = DynamoDBDocumentClient.from(client);
+
+module.exports = { dynamo };
